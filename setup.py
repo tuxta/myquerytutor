afe6eb20af5f7bfc0e910146db3fb14db2c1f01d
tuxta <tuxta2@gmail.com>
2018-10-21 10:52:37 +1000

    Added cx_Freeze setup file for building Mac APP

---
+++ setup.py
@@ -0,0 +1,19 @@
+from cx_Freeze import setup, Executable
+
+execs = [Executable("main.py", icon="mqt.icns")]
+include_files = ["MQT_APP.sqlite", "MQT_EX.sqlite"]
+
+setup(
+    name = "My Query Tutor",
+    version = "1.0",
+    description = "A personal tutor to teach you Structured Query Language",
+    options =
+    {
+        "build_exe":
+        {
+            "include_files":include_files,
+            "excludes":['tcl', 'ttk', 'tkinter', 'Tkinter']
+        }
+    },
+    executables=execs
+)
